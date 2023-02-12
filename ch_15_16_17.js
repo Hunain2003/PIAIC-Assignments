f30b1922bd5e85e8349a27e8e94273ebddea87ae
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-02-12 12:50:45 +0500

    Update ch_15_16_17.js

--- ch_15_16_17.js
+++ ch_15_16_17.js
@@ -2,13 +2,13 @@ let courses = [];
 
 let course1 = prompt("course 1 ", "computer");
 courses.push(course1);
-let course2 = prompt("course 2 ", "computer");
+let course2 = prompt("course 2 ", "python");
 courses.push(course2);
-let course3 = prompt("course 3 ", "computer");
+let course3 = prompt("course 3 ", "java");
 courses.push(course3);
-let course4 = prompt("course 4 ", "computer");
+let course4 = prompt("course 4 ", "docker");
 courses.push(course4);
-let course5 = prompt("course 5 ", "computer");
+let course5 = prompt("course 5 ", "html");
 courses.push(course5);
 
 alert(courses)
