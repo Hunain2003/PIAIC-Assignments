9334a4378d4488719cf39a0c3b788874e7285bd4
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-02-12 12:56:46 +0500

    Update ch_15_16_17.js

--- ch_15_16_17.js
+++ ch_15_16_17.js
@@ -14,9 +14,14 @@ courses.push(course5);
 alert(courses)
 
 course1 = prompt("course 1 ", course1);
+courses.splice(0, 1, course1);
 course2 = prompt("course 2 ", course2);
+courses.splice(1, 1, course2);
 course3 = prompt("course 3 ", course3);
+courses.splice(2, 1, course3);
 course4 = prompt("course 4 ", course4);
+courses.splice(3, 1, course4);
 course5 = prompt("course 5 ", course5);
+courses.splice(4, 1, course5);
 
 alert(courses);
