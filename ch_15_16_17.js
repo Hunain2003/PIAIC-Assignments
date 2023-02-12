63ca18f173550545ec1434355571e73ce0065de6
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-02-12 12:49:58 +0500

    Update ch_15_16_17.js

--- ch_15_16_17.js
+++ ch_15_16_17.js
@@ -1,7 +1,22 @@
 let courses = [];
-for (let i = 0; i < 5; i++)
-{
-  courses.push(prompt("Course Name"))
-}
+
+let course1 = prompt("course 1 ", "computer");
+courses.push(course1);
+let course2 = prompt("course 2 ", "computer");
+courses.push(course2);
+let course3 = prompt("course 3 ", "computer");
+courses.push(course3);
+let course4 = prompt("course 4 ", "computer");
+courses.push(course4);
+let course5 = prompt("course 5 ", "computer");
+courses.push(course5);
+
+alert(courses)
+
+course1 = prompt("course 1 ", "computer");
+course2 = prompt("course 2 ", "computer");
+course3 = prompt("course 3 ", "computer");
+course4 = prompt("course 4 ", "computer");
+course5 = prompt("course 5 ", "computer");
 
 alert(courses);
