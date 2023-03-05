ee4c09a990c4612feaae0461e20b3e0df9c8c0ca
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-03-05 20:25:23 +0500

    Update ch_21_to_30.js

--- ch_21_to_30.js
+++ ch_21_to_30.js
@@ -1,10 +1,13 @@
 let userInput = Math.ceil(parseInt(prompt("Enter a number")));
 var text = "This is my dummy text";
 
-var sliceText = text.slice(0, userInput);
+var sliceText = text.slice(0, userInput + 1);
 
 var reversedText = "";
 
-for (let i = userInput; i > 0; i--)
+for (let i = userInput; i >= 0; i--)
+{
+  reversedText += sliceText[i]
+}
 
 console.log(reversedText);
